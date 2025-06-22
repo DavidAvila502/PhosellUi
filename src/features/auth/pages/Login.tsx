32cc1b2932e1aa65591e42a1485c8bb6d9dbcbce
David Avila <davidavila6002@gmail.com>
2025-06-22 16:00:59 -0600

    login image added .

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { ROLES } from "../../../app/constants/roles";
 import ClassicButton from "../../../shared/components/ClassicButton";
 import ClassicLabelField from "../../../shared/components/ClassicLabelField";
+import loginImage from "../assets/login_image.jpg";
 
 export function Login() {
    const navigate = useNavigate();
@@ -39,53 +40,70 @@ export function Login() {
    }, [data, navigate]);
 
    return (
-      <div className="flex flex-col items-center justify-center min-h-[100vh]">
+      <div
+         className={`flex flex-col items-center justify-center min-h-[calc(100vh-70px)]`}
+      >
          <div
-            className="bg-white flex items-center rounded-[20px] min-h-[300px] px-[40px] py-[60px]
-                        justify-center flex-col border-solid border-2 border-gray-200 shadow-2xl
-                        w-[350px] max-[450px]:w-[90%] sm:w-[400px]
-                        "
+            className="grid grid-cols-[1fr_2fr] rounded-[20px] h-[500px] w-[900px] border-2 border-gray-100 shadow-xl
+                  max-lg:w-[700px] max-md:grid-cols-[0fr_1fr] max-md:w-[70%] max-sm:w-[90%]"
          >
-            <form
-               onSubmit={handleSubmit}
-               className="flex flex-col gap-9 w-full"
-            >
-               <p className="text-blue-400 font-bold text-2xl">
-                  Iniciar sesión
-               </p>
-
-               <div className="flex flex-col gap-2 items-start">
-                  <ClassicLabelField
-                     id="emial"
-                     name="email"
-                     type="email"
-                     value={credentials.email}
-                     textLabel="Email"
-                     onChange={handleChange}
-                     required={true}
-                  />
-               </div>
-
-               <div className="flex flex-col gap-2 items-start">
-                  <ClassicLabelField
-                     id="password"
-                     name="password"
-                     type="password"
-                     value={credentials.password}
-                     textLabel="Password"
-                     onChange={handleChange}
-                     required={true}
-                  />
-               </div>
-
-               <ClassicButton
-                  text="Enviar"
-                  color="bg-blue-400"
-                  type="submit"
-                  style="rounded-[10px]"
-                  isLoading={isLoading}
+            <div className="relative w-full h-full">
+               <img
+                  src={loginImage}
+                  alt="loginImage"
+                  className="absolute inset-0 object-cover h-full w-full rounded-tl-[20px] rounded-bl-[20px]"
                />
-            </form>
+            </div>
+
+            <div className="w-full h-full bg-white flex items-center justify-center rounded-[20px]">
+               {/* Login form */}
+
+               <div
+                  className="bg-white flex items-center rounded-br-[20px] rounded-tr-[20px] h-full px-[40px] py-[60px]
+                        justify-center flex-col w-[350px] max-sm:w-full"
+               >
+                  <form
+                     onSubmit={handleSubmit}
+                     className="flex flex-col gap-9 w-full"
+                  >
+                     <p className="text-blue-400 font-bold text-2xl">
+                        Iniciar sesión
+                     </p>
+
+                     <div className="flex flex-col gap-2 items-start">
+                        <ClassicLabelField
+                           id="emial"
+                           name="email"
+                           type="email"
+                           value={credentials.email}
+                           textLabel="Email"
+                           onChange={handleChange}
+                           required={true}
+                        />
+                     </div>
+
+                     <div className="flex flex-col gap-2 items-start">
+                        <ClassicLabelField
+                           id="password"
+                           name="password"
+                           type="password"
+                           value={credentials.password}
+                           textLabel="Password"
+                           onChange={handleChange}
+                           required={true}
+                        />
+                     </div>
+
+                     <ClassicButton
+                        text="Enviar"
+                        color="bg-blue-400"
+                        type="submit"
+                        style="rounded-[10px]"
+                        isLoading={isLoading}
+                     />
+                  </form>
+               </div>
+            </div>
          </div>
       </div>
    );
