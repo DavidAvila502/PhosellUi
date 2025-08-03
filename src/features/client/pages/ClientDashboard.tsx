2cd3ee47b3a5bd57d32e197fd5a162ac3ac140d5
David Avila <davidavila6002@gmail.com>
2025-08-03 14:52:46 -0600

    SessionCard

--- src/features/client/pages/ClientDashboard.tsx
+++ src/features/client/pages/ClientDashboard.tsx
@@ -1,9 +1,40 @@
+import { useEffect } from "react";
+import useGetSessionsMeClient from "../../session/hooks/useGetSessionsMeClient";
+import SessionListContainer from "../../session/components/SessionListContainer";
+import SessionCard from "../../session/components/SessionCard";
+import React from "react";
+
 export function ClientDashboard() {
+   const { clientSessionsData, clientSessionsDataError, getSessionsMeClient } =
+      useGetSessionsMeClient();
+
+   useEffect(() => {
+      getSessionsMeClient();
+   }, [getSessionsMeClient]);
+
+   useEffect(() => {
+      if (clientSessionsDataError) console.warn(clientSessionsDataError);
+   }, [clientSessionsDataError]);
+
    return (
-      <div className="flex flex-col items-center justify-center min-h-[100vh]">
-         <div className="max-w-[1536px] w-full p-[8px]">
-            <div className="max-w-[1280px] mx-auto w-full flex justify-center">
-               <p>Hellouda</p>
+      <div className="flex flex-col items-center min-h-[100vh] bg-gray-50">
+         <div className="max-w-[1536px] w-full p-[20px]">
+            <div className="max-w-[1280px] mx-auto w-full pt-[40px] flex flex-col">
+               <p className="mb-[20px] font-bold text-blue-400 text-[25px]">
+                  Mis Sesiones
+               </p>
+
+               <SessionListContainer>
+                  {clientSessionsData == null ||
+                  clientSessionsData.content.length == 0 ? (
+                     <p>Parece que no tienes sesiones todavía</p>
+                  ) : null}
+                  {clientSessionsData?.content.map((s, index) => (
+                     <React.Fragment key={index}>
+                        <SessionCard session={s} />
+                     </React.Fragment>
+                  ))}
+               </SessionListContainer>
             </div>
          </div>
       </div>
--- src/features/session/components/SessionCard.tsx
+++ src/features/session/components/SessionCard.tsx
@@ -1,3 +1,64 @@
-const SessionCard = () => {};
+import ClassicButton from "../../../shared/components/ClassicButton";
+import type { Session } from "../models/sessionModels";
+import {
+   getSessionStatusColor,
+   getStringOfSessionSessionStatus,
+} from "../utils/SessionStatusUtils";
+import { formatRawTime } from "../utils/TimeUtils";
+
+interface SessionCardProps {
+   session: Session;
+}
+
+const SessionCard = ({ session }: SessionCardProps) => {
+   return (
+      <div className="w-full rounded-[20px] bg-white h-[280px] border-1 border-gray-400">
+         <div
+            className={`p-5 rounded-tl-[20px] rounded-tr-[20px] flex flex-row justify-between ${getSessionStatusColor(
+               session.sessionStatus
+            )}`}
+         >
+            <p className="text-white font-bold truncate">
+               Fecha: {session.sessionDate}
+            </p>
+
+            <div className="bg-white rounded-2xl px-3 text-[17px] font-medium text-blue-400">
+               {getStringOfSessionSessionStatus(session.sessionStatus)}
+            </div>
+         </div>
+
+         <div className="flex flex-col gap-3 px-5 pt-2">
+            <div className="flex flex-row gap-3">
+               <p className="font-bold">Hora:</p>{" "}
+               <p className="truncate">{formatRawTime(session.sessionTime)}</p>
+            </div>
+
+            <div className="flex flex-row gap-3">
+               <p className="font-bold">Lugar:</p>{" "}
+               <p className="truncate">{session.location}</p>
+            </div>
+
+            <div className="flex flex-row gap-3">
+               <p className="font-bold">Paquete:</p>{" "}
+               <p className="truncate">{`${session.sessionPackage.name} ($${session.sessionPackage.price} MXN)`}</p>
+            </div>
+
+            <div className="flex flex-row gap-3">
+               <p className="font-bold">Fotografo:</p>{" "}
+               <p className="truncate">{session.photographer.fullName}</p>
+            </div>
+
+            <div className="flex flex-row justify-end">
+               <ClassicButton
+                  type="button"
+                  text="Ver"
+                  style="w-[100px]"
+                  color="bg-blue-400"
+               />
+            </div>
+         </div>
+      </div>
+   );
+};
 
 export default SessionCard;
